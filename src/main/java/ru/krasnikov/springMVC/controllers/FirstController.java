be14f6f4f4f6bcaba4708048a5f55717d4d6cc30
aleksandr_krasnikov_1 <aa_krasnikov@mail.ru>
2023-07-04 23:03:56 +0400

    new vers (develop)

--- src/main/java/ru/krasnikov/springMVC/controllers/FirstController.java
+++ src/main/java/ru/krasnikov/springMVC/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.krasnikov.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +13,11 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam("name") String name,
-                            @RequestParam("surname") String surname) {
-        System.out.println(name + surname);
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message", name + surname);
 
         return "first/hello";
     }
